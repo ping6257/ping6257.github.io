614a08b138079aee918606fd94531619a9ac0520
ping6257 <34451459+ping6257@users.noreply.github.com>
2018-01-07 22:31:53 +0800

    js_test
    js_test

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+function openSlideMenu() {
+    document.getElementById('side_menu').style.width='250px';
+    document.getElementsByClassName('menu_li').style.marginLeft='3000px';
+
+
+}
+function closeSideMenu() {
+    document.getElementById('side_menu').style.width='0px';
+    document.getElementById('main').style.marginLeft='0px';
+
+}
+
+
