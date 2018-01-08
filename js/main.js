81121f9fe02cbcea641ab16de3f4325436263120
ping6257 <34451459+ping6257@users.noreply.github.com>
2018-01-08 17:20:56 +0800

    renew

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
 
+
 function openSlideMenu() {
-    document.getElementById('side_menu').style.width='250px';
-    document.getElementsByClassName('menu_li').style.marginLeft='3000px';
 
-
+    document.getElementById('side_menu').style.width='1000px';
+    document.getElementById('main').style.marginLeft='1500px';
 }
+
 function closeSideMenu() {
-    document.getElementById('side_menu').style.width='0px';
-    document.getElementById('main').style.marginLeft='0px';
-
+    document.getElementById('side_menu').style.width='0';
+    document.getElementById('main').style.marginLeft='0';
 }
-
-
